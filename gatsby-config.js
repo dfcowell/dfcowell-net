772c8f57e1ccb3f4163c4e0b105c0fb87f539d53
Dan Cowell <admin@dfcowell.net>
2019-06-16 11:51:39 +0900

    site meta

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ var proxy = require('http-proxy-middleware')
 
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: 'Dan Cowell | Front End Engineer',
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'Personal website, blog and projects of Dan Cowell.',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
